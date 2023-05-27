2998bb8b05d42ef9c43d668dd11ba7e47a8fe583
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-05-26 20:26:05 -0700

    It opens a window now. Wtf.

--- main.py
+++ main.py
@@ -1,61 +1,56 @@
-import openai
-import os
-from dotenv import load_dotenv
 import tkinter as tk
-import time
 
-# Load environment variables
-load_dotenv()
+class Game:
+    def __init__(self, master, total_size=10, view_size=5):
+        self.total_size = total_size
+        self.view_size = view_size
+        self.user_pos = [total_size//2, total_size//2]
+        self.grid = [['.' for _ in range(total_size)] for _ in range(total_size)]
+        self.grid[self.user_pos[0]][self.user_pos[1]] = '@'
 
-# Set OpenAI API key
-openai.api_key = os.getenv('OPENAI_API_KEY')
+        # Initialize Tkinter grid
+        self.labels = [[tk.Label(master) for _ in range(view_size)] for _ in range(view_size)]
+        for i in range(view_size):
+            for j in range(view_size):
+                self.labels[i][j].grid(row=i, column=j)
 
-# Create a Tkinter window
+        self.update_view()
+
+    def update_view(self):
+        upper_bound = max(0, self.user_pos[0]-self.view_size//2)
+        lower_bound = min(self.total_size, upper_bound+self.view_size)
+        upper_bound = max(0, lower_bound-self.view_size)
+        left_bound = max(0, self.user_pos[1]-self.view_size//2)
+        right_bound = min(self.total_size, left_bound+self.view_size)
+        left_bound = max(0, right_bound-self.view_size)
+
+        for i in range(upper_bound, lower_bound):
+            for j in range(left_bound, right_bound):
+                self.labels[i-upper_bound][j-left_bound].config(text=self.grid[i][j])
+
+    def move(self, direction):
+        new_pos = self.user_pos.copy()
+
+        if direction == 'w':  # Up
+            new_pos[0] -= 1
+        elif direction == 's':  # Down
+            new_pos[0] += 1
+        elif direction == 'a':  # Left
+            new_pos[1] -= 1
+        elif direction == 'd':  # Right
+            new_pos[1] += 1
+
+        if 0 <= new_pos[0] < self.total_size and 0 <= new_pos[1] < self.total_size:
+            self.grid[self.user_pos[0]][self.user_pos[1]] = '.'  # Remove old position
+            self.user_pos = new_pos
+            self.grid[self.user_pos[0]][self.user_pos[1]] = '@'  # Update new position
+
+        self.update_view()
+
+def on_key_press(event, game):
+    game.move(event.char)
+
 root = tk.Tk()
-response_label = tk.Label(root)
-response_label.pack()
-
-def stream_to_console(message):
-    for char in message:
-        print(char, end='', flush=True)
-        time.sleep(0.035)
-    print()  # newline at the end
-
-def stream_to_label(message, index=0):
-    # Add one character to the label text and schedule the next character
-    if index < len(message):
-        response_label["text"] += message[index]
-        root.after(35, stream_to_label, message, index + 1)
-
-def stream_ai_response(user_input):
-    # Call to the OpenAI API
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": user_input}
-        ]
-    )
-
-    # Prepare response message
-    response_message = f"AI's response: {response['choices'][0]['message']['content']}"
-
-    # Stream to console
-    stream_to_console(response_message)
-
-    # Clear the previous response and stream the new response to the label
-    response_label["text"] = ""
-    stream_to_label(response_message)
-
-def run_stream():
-    user_input = user_input_entry.get()
-    stream_ai_response(user_input)
-
-# Create a Tkinter entry field and a submit button
-user_input_entry = tk.Entry(root)
-user_input_entry.pack()
-
-submit_button = tk.Button(root, text="Submit", command=run_stream)
-submit_button.pack()
-
+game = Game(root)
+root.bind('<Key>', lambda event: on_key_press(event, game))
 root.mainloop()
