be2e3cf12b2f5a29b65abc6b9e2b7b4448174062
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-05-26 20:27:34 -0700

    The game is now more visible and modular.

--- game.py
+++ game.py
@@ -1,23 +1,30 @@
+import tkinter as tk
+
 class Game:
-    def __init__(self, total_size=10, view_size=5):
+    def __init__(self, master, total_size=10):
         self.total_size = total_size
-        self.view_size = view_size
-        self.user_pos = [total_size//2, total_size//2]  # Starting position
-        self.grid = [['.' for _ in range(total_size)] for _ in range(total_size)]  # Initialize environment
-        self.grid[self.user_pos[0]][self.user_pos[1]] = '@'  # Place user
+        self.user_pos = [total_size//2, total_size//2]
+        self.grid = [[' ' for _ in range(total_size)] for _ in range(total_size)]
 
-    def print_view(self):
-        upper_bound = max(0, self.user_pos[0]-self.view_size//2)
-        lower_bound = min(self.total_size, upper_bound+self.view_size)
-        upper_bound = max(0, lower_bound-self.view_size)
-        left_bound = max(0, self.user_pos[1]-self.view_size//2)
-        right_bound = min(self.total_size, left_bound+self.view_size)
-        left_bound = max(0, right_bound-self.view_size)
+        # Initialize walls (We're just creating a simple box for this example)
+        for i in range(total_size):
+            self.grid[i][0] = self.grid[i][total_size-1] = '#'
+            self.grid[0][i] = self.grid[total_size-1][i] = '#'
 
-        for i in range(upper_bound, lower_bound):
-            for j in range(left_bound, right_bound):
-                print(self.grid[i][j], end=' ')
-            print()
+        self.grid[self.user_pos[0]][self.user_pos[1]] = '@'
+
+        # Initialize Tkinter grid
+        self.labels = [[tk.Label(master, width=2, borderwidth=1) for _ in range(total_size)] for _ in range(total_size)]
+        for i in range(total_size):
+            for j in range(total_size):
+                self.labels[i][j].grid(row=i, column=j)
+
+        self.update_view()
+
+    def update_view(self):
+        for i in range(self.total_size):
+            for j in range(self.total_size):
+                self.labels[i][j].config(text=self.grid[i][j])
 
     def move(self, direction):
         new_pos = self.user_pos.copy()
@@ -31,15 +38,18 @@ class Game:
         elif direction == 'd':  # Right
             new_pos[1] += 1
 
-        if 0 <= new_pos[0] < self.total_size and 0 <= new_pos[1] < self.total_size:
-            self.grid[self.user_pos[0]][self.user_pos[1]] = '.'  # Remove old position
+        # Add wall collision check
+        if self.grid[new_pos[0]][new_pos[1]] != '#':
+            self.grid[self.user_pos[0]][self.user_pos[1]] = ' '  # Remove old position
             self.user_pos = new_pos
             self.grid[self.user_pos[0]][self.user_pos[1]] = '@'  # Update new position
 
-game = Game()
-game.print_view()
+        self.update_view()
 
-while True:
-    move = input("Enter movement (w/a/s/d): ")
-    game.move(move)
-    game.print_view()
+def on_key_press(event, game):
+    game.move(event.char)
+
+root = tk.Tk()
+game = Game(root)
+root.bind('<Key>', lambda event: on_key_press(event, game))
+root.mainloop()
