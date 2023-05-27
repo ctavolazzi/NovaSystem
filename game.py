16a735a6f21cf15316b7c6b7963cf0d42f590ae6
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-05-26 22:07:24 -0700

    It almost works...

--- game.py
+++ game.py
@@ -1,20 +1,29 @@
 import tkinter as tk
+import random
 from robot import Robot
 
 class Game:
     def __init__(self, master, total_size=10):
+        self.master = master
         self.total_size = total_size
         self.user_pos = [total_size//2, total_size//2]
         self.grid = [[' ' for _ in range(total_size)] for _ in range(total_size)]
+        self.paused = False
 
-        # Initialize walls (We're just creating a simple box for this example)
+        robot_x_pos, robot_y_pos = self.user_pos
+        while [robot_x_pos, robot_y_pos] == self.user_pos:
+            robot_x_pos = random.randint(1, total_size-2)
+            robot_y_pos = random.randint(1, total_size-2)
+
+        self.robot = Robot('Robbie', 'A friendly robot', robot_x_pos, robot_y_pos, self)
+
         for i in range(total_size):
             self.grid[i][0] = self.grid[i][total_size-1] = '#'
             self.grid[0][i] = self.grid[total_size-1][i] = '#'
 
         self.grid[self.user_pos[0]][self.user_pos[1]] = '@'
+        self.grid[self.robot.x_pos][self.robot.y_pos] = self.robot.ascii_art
 
-        # Initialize Tkinter grid
         self.labels = [[tk.Label(master, width=2, borderwidth=1) for _ in range(total_size)] for _ in range(total_size)]
         for i in range(total_size):
             for j in range(total_size):
@@ -28,8 +37,9 @@ class Game:
                 self.labels[i][j].config(text=self.grid[i][j])
 
     def move(self, direction):
+        if self.paused:
+            return
         new_pos = self.user_pos.copy()
-
         if direction == 'w':  # Up
             new_pos[0] -= 1
         elif direction == 's':  # Down
@@ -39,18 +49,20 @@ class Game:
         elif direction == 'd':  # Right
             new_pos[1] += 1
 
-        # Add wall collision check
-        if self.grid[new_pos[0]][new_pos[1]] != '#':
-            self.grid[self.user_pos[0]][self.user_pos[1]] = ' '  # Remove old position
-            self.user_pos = new_pos
-            self.grid[self.user_pos[0]][self.user_pos[1]] = '@'  # Update new position
+        if self.grid[new_pos[0]][new_pos[1]] == 'R':
+            self.robot.open_chat_window()
+            return
 
+        self.grid[self.user_pos[0]][self.user_pos[1]] = ' '
+        self.grid[new_pos[0]][new_pos[1]] = '@'
+        self.user_pos = new_pos
         self.update_view()
 
-def on_key_press(event, game):
-    game.move(event.char)
+    def pause(self):
+        self.paused = True
 
-root = tk.Tk()
-game = Game(root)
-root.bind('<Key>', lambda event: on_key_press(event, game))
-root.mainloop()
+    def resume(self):
+        self.paused = False
+
+    def quit(self):
+        self.master.destroy()
