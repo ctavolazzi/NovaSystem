d8f9240e613dfd0efc3eb7fae53373dcd96b6ccd
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-06-01 15:37:23 -0700

    My Django App is broken

--- Versions/Tomato_0-1/winfo/polls/urls.py
+++ Versions/Tomato_0-1/winfo/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
