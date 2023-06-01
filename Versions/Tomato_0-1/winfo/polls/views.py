a399b2feecd430bc5d2189664298182672854694
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-06-01 15:37:23 -0700

    My Django App is broken

--- Versions/Tomato_0-1/winfo/polls/views.py
+++ Versions/Tomato_0-1/winfo/polls/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-
-# Leave the rest of the views (detail, results, vote) unchanged
-
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
