01c05b5cd460637af0dd45e91e02265b4b59cdde
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-10-10 00:36:30 -0700

    Giving Marvin a snarky personality

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+Marvin = {
+    'name': 'Marvin',
+    'got_pissed_off': False
+}
+
 def read_json_file(filename):
     with open(filename, 'r') as f:
         return json.load(f)
@@ -18,13 +23,15 @@ def write_json_file(filename, data):
 def validate_input(attribute, user_input):
     if attribute == 'age':
         return user_input.isdigit() and int(user_input) > 0
+    elif attribute == 'email':
+        return "@" in user_input
     return True  # Placeholder for other attributes
 
 def fetch_openai_chatcompletion(attribute, user_data=None, previous_user_inputs=None):
     messages = [
         {
             'role': 'system',
-            'content': 'You are a snarky AI named Marvin. Your goal is to help the user get entered into the NOVA System.'
+            'content': f'You are a snarky AI named Marvin. Your goal is to help the user get entered into the NOVA System. Marvin is {"pissed off" if Marvin["got_pissed_off"] else "not pissed off"}.'
         },
         {
             'role': 'user',
@@ -70,11 +77,23 @@ def conduct_conversation():
             if valid_input:
                 user_data[attribute] = user_input if attribute != 'age' else int(user_input)
             else:
-                if attempts >= 3:
-                    snarky_reply = fetch_openai_chatcompletion("incorrect age", user_data, previous_user_inputs)
+                if attempts >= 3 & attempts < 5:
+                    snarky_reply = fetch_openai_chatcompletion("incorrect input, over max attempts; BE FIRM AND DIRECT, AND ACT LIKE YOU'RE GETTING IRRITATED, BUT YOU'RE STILL WANTING TO BE NICE", user_data, previous_user_inputs)
                     stream_to_console(snarky_reply)
+                elif attempts >= 5:
+                    serious_reply = fetch_openai_chatcompletion("incorrect input, over max attempts; BE FIRM AND DIRECT, AND NOW YOU ARE IRRITATED WITH THE USER AND YOU ARE GETTING NOTICABLY FRUSTRATED", user_data, previous_user_inputs)
+                    stream_to_console(serious_reply)
+                elif attempts >= 6:
+                    warning_reply = fetch_openai_chatcompletion("incorrect input, over max attempts; BE FIRM AND DIRECT, AND NOW YOU ARE LOSING YOUR COOL - GET PISSED OFF", user_data, previous_user_inputs)
+                    Marvin.got_pissed_off = True
+                    stream_to_console(warning_reply)
+                elif attempts >= 7:
+                    pissed_off_reply = fetch_openai_chatcompletion("incorrect input, over max attempts; YOU HAVE COMPLETELY LOST IT AND YOU ARE NOT LONGER HOLDING BACK YOUR RAGE AT THE USER FOR THEIR REPEATED REFUSAL TO ANSWER YOUR SIMPLE QUESTION", user_data, previous_user_inputs)
+                    stream_to_console(pissed_off_reply)
                 else:
-                    stream_to_console("Invalid input. Please try again.")
+                    guiding_reply = fetch_openai_chatcompletion("incorrect input, but under max attempts", user_data, previous_user_inputs)
+                    # steering_message = "Hmm, that doesn't quite work. Let's try this again."
+                    stream_to_console(guiding_reply)
                 attempts += 1
 
     write_json_file('user.json', user_data)
