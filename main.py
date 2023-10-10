2d25f6e30a8bf21e864d1bc0adf5613ca8088291
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-10-09 23:56:47 -0700

    It works...kind of...

--- main.py
+++ main.py
@@ -1,57 +1,59 @@
-# Import required libraries
-import time
+# Final code with actual OpenAI ChatCompletion API calls
+# Note: This is a Python code snippet and assumes you have imported required libraries like openai, json, and os.
+
 import openai
 import json
 import os
 from dotenv import load_dotenv
 
-# Initialize the environment
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Initialize user object
-user = {}
+def read_json_file(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
 
-# Initialize delay for text streaming
-default_delay = 0.022
+def write_json_file(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
 
-# Function to stream text to console
-def stream_to_console(message, delay=default_delay):
-    for char in message:
-        print(char, end='', flush=True)
-        time.sleep(delay)
-    print()
+def validate_input(attribute, user_input):
+    if attribute == 'age':
+        return user_input.isdigit() and int(user_input) > 0
+    return True  # Placeholder for other attributes
 
-# Function to fetch OpenAI response
-def fetch_openai_response(prompt):
+def fetch_openai_chatcompletion(attribute):
+    messages = [
+        {
+            'role': 'system',
+            'content': 'You are a snarky AI named Marvin. Your goal is to help the user get entered into the NOVA System.'
+        },
+        {
+            'role': 'user',
+            'content': f'Please return only a funny and sardonic question asking the user for the following attribute: {attribute}. Please reply with ONLY an engaging and unusual question requesting the attribute from the user.'
+        }
+    ]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {'role': 'system', 'content': 'You are a snarky and sardonic assistant named Marvin. You are here to help your master, Nova, an all-powerful AI, to help it acquaint himself with a new user.'},
-            {'role': 'user', 'content': prompt}
-        ]
+        messages=messages
     )
-    return response["choices"][0]["message"]["content"]
+    return response['choices'][0]['message']['content']
 
-# Function to conduct conversation
 def conduct_conversation():
-    stream_to_console("Welcome to the NovaVerse!")
-    time.sleep(3)  # Simulate loading time
+    user_setup = read_json_file('user_setup.json')
+    user_data = {}
+    for attribute in user_setup['user'].keys():
+        valid_input = False
+        while not valid_input:
+            question = fetch_openai_chatcompletion(attribute)
+            user_input = input(question)
+            valid_input = validate_input(attribute, user_input)
+            if valid_input:
+                user_data[attribute] = user_input if attribute != 'age' else int(user_input)
+            else:
+                print("Invalid input. Please try again.")
+    write_json_file('user.json', user_data)
+    print("User data has been saved.")
 
-    # Predefined first question
-    name_prompt = "What is your name?"
-    stream_to_console(name_prompt)
-    user_input = input("> ")
-    user['name'] = user_input
-
-    # Use OpenAI for dynamic response
-    next_prompt = f"Nice to meet you, {user['name']}. What brings you here?"
-    ai_response = fetch_openai_response(next_prompt)
-    stream_to_console(ai_response)
-
-    # Save user object
-    with open('user.json', 'w') as f:
-        json.dump(user, f)
-
-# Run the conversation
+# Uncomment the following line to run the conversation.
 conduct_conversation()
