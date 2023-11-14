ae353f966724751843f49798da82b2b2d72fcf32
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-11-13 20:13:12 -0800

    init NovaGPT Iteration 1 Pre-Alpha Version

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+def stc(message):
+  # Stream each character in a message to the console
+  for char in message:
+    print(char, end="")
+    time.sleep(0.1)
+  print()
+
+def main():
+  stc("Hello World!")
+
+if __name__ == "__main__":
+  main()
