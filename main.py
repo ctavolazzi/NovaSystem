e58bf5db1615c27d4e24861d0001195889778658
Christopher Tavolazzi <ctavolazzi@gmail.com>
2023-11-28 18:51:03 -0800

    Init support for Flask...it's time to open this bitch up

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Welcome\nto the\nNovaSystem")
+# main.py
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        # Process the data, save it, send an email, etc.
+        return f"Thank you, {name}. We received your message!"
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
