ea2642cbf1c1cc011781b249cfc3e60de1ebf4f2
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-03-06 15:38:34 -0800

    Init working OpenAI Chat Example in Svelte

---
+++ novasystem-vercel-ai/src/routes/api/chat/+server.js
@@ -0,0 +1,29 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { OPENAI_API_KEY } from '$env/static/private';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
+ 
+// Set the runtime to edge for best performance
+export const config = {
+  runtime: 'edge'
+};
+ 
+export async function POST({ request }) {
+  const { messages } = await request.json();
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,a
+    messages,
+  });
+ 
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
