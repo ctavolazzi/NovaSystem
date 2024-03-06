d6332c1d1ab4f226ee05b045455ebcaf4ab923c2
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-03-06 12:35:45 -0800

    Update to Node 18

--- novasystem-svelte-docker/src/App.svelte
+++ novasystem-svelte-docker/src/App.svelte
@@ -6,6 +6,8 @@
 
 <main>
 	<Viewport {content} />
+	<img src="/Users/ctavolazzi/Code/NovaSystem/novasystem_mockup.jpeg" alt="NovaSystemMockup" />
+	<img src="https://e7.pngegg.com/pngimages/435/537/png-clipart-computer-icons-hyperlink-direct-link-others-miscellaneous-text.png" alt="Rowan Berries" />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<p>Welcome to the Dockerized NovaSystem application.</p>
