e5c1e4e891118441be7143c8cf324b7df6d39a31
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-03-06 17:29:44 -0800

    Init WInston logging

--- novasystem-vercel-ai/src/routes/api/chat/+server.js
+++ novasystem-vercel-ai/src/routes/api/chat/+server.js
@@ -1,7 +1,17 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { OPENAI_API_KEY } from '$env/static/private';
- 
+
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: 'logs.txt' }),
+  ],
+});
+
+logger.info('This is a log message!');
+
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
@@ -14,6 +24,14 @@ export const config = {
  
 export async function POST({ request }) {
   const { messages } = await request.json();
+  console.log('messages', messages);
+  logger.info(`messages: , ${JSON.stringify(messages)}`);
+
+  // Save the messages to a file
+  // const file = await Deno.open('messages.txt', { write: true, create: true, truncate: true });
+  // await file.write(new TextEncoder().encode(JSON.stringify(messages)));
+  // file.close();
+  // This doesn't work, but it's a start
  
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
@@ -22,8 +40,14 @@ export async function POST({ request }) {
     messages,
   });
  
+  // console.log('response', response);
+  // for await (const chunk of response) {
+  //   console.log('chunk', chunk);
+  // }
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
   // Respond with the stream
+
   return new StreamingTextResponse(stream);
+
 }
