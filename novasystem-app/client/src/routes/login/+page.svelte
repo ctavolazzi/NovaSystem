48a793fa7bef586d557d4515578a92136d1110e4
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-03-10 10:54:39 -0700

    Init novasystem-app

---
+++ novasystem-app/client/src/routes/login/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import DiCode from 'svelte-icons/di/DiCode.svelte';
+</script>
+
+<div class="flex flex-col items-center h-full w-full pt-12">
+	<div class="h-24 w-24 text-primary">
+		<DiCode />
+	</div>
+	<h2 class="mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
+		Sign in to your account
+	</h2>
+	<p class="text-center mt-1">
+		Or <a href="/login" class="text-primary font-medium hover:cursor-pointer">register</a> if you don't
+		already have an account.
+	</p>
+	<div class="bg-white py-6 shadow-md rounded-lg w-full max-w-sm mt-6">
+		<form method="POST" action="?/login" class="flex flex-col items-center space-y-2 w-full">
+			<div class="form-control w-full max-w-xs">
+				<label for="email" class="label font-medium pb-1">
+					<span class="label-text">Email</span>
+				</label>
+				<input type="email" name="email" class="input input-bordered w-full max-w-xs" />
+			</div>
+			<div class="form-control w-full max-w-xs">
+				<label for="password" class="label font-medium pb-1">
+					<span class="label-text">Password</span>
+				</label>
+				<input type="password" name="password" class="input input-bordered w-full max-w-xs" />
+			</div>
+			<div class="w-full max-w-xs pt-3">
+				<button class="btn btn-primary w-full max-w-xs">Login</button>
+			</div>
+		</form>
+	</div>
+</div>
