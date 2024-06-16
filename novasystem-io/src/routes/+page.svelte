497a475259d06588d321f1a7b63a6c17d715a8ef
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-06-16 14:02:18 -0700

    Testing cross-domain integration

--- novasystem-io/src/routes/+page.svelte
+++ novasystem-io/src/routes/+page.svelte
@@ -1,2 +1,26 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<script>
+    import { onMount } from 'svelte';
+    
+    let backendData = '';
+    
+    onMount(async () => {
+      try {
+        const response = await fetch('https://novasystem-backend-5bc4a8329298.herokuapp.com/');
+        backendData = await response.text();
+      } catch (error) {
+        console.error('Error fetching data from the backend:', error);
+      }
+    });
+    </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if backendData}
+  <p>Data from the backend: {backendData}</p>
+{:else}
+  <p>Loading data from the backend...</p>
+{/if}
