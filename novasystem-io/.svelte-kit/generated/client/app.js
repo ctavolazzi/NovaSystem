f7f9dcf2ab142667a1bc244fb21a9e3da7ada834
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-06-16 11:50:54 -0700

    Auto Deploy to Cloudflare

--- novasystem-io/.svelte-kit/generated/client/app.js
+++ novasystem-io/.svelte-kit/generated/client/app.js
@@ -3,13 +3,15 @@ export { matchers } from './matchers.js';
 export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
-	() => import('./nodes/2')
+	() => import('./nodes/2'),
+	() => import('./nodes/3')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2]
+		"/": [2],
+		"/test": [3]
 	};
 
 export const hooks = {
